991ed0e67f75f1bf887348de63d79b1044a70a86
Apoorva Singh <apoorvasingh157@gmail.com>
2019-01-06 16:50:40 +0530

    Accepts constructor arguments for dotenv file path or upstox api credentials.

--- tickerstore/store.py
+++ tickerstore/store.py
@@ -1,6 +1,7 @@
 from upstox_api.api import *
 from . import daemon as daemon
 from tickerstore.errors import SourceError
+from dotenv import load_dotenv
 import nsepy
 import datetime
 import pathlib
@@ -9,7 +10,6 @@ import time
 import json
 import math
 import crayons
-import pandas
 
 
 class TickerStore:
@@ -29,9 +29,18 @@ class TickerStore:
     def __init__(self, **kwargs):
         self.fetch_order = [TickerStore.UPSTOX, TickerStore.NSE]  # Default fetch order
 
-        if "env_file" in kwargs:
-            # Load everything from the environment file
-            pass
+        # Load the values from .env files to Enviroment variable
+        if "dotenv" in kwargs:
+            load_dotenv(dotenv_path=kwargs["dotenv"])
+
+        if (
+            "upstox_api_key" in kwargs.keys()
+            and "upstox_api_secret" in kwargs.keys()
+            and "upstox_redirect_uri" in kwargs.keys()
+        ):
+            os.environ["UPSTOX_API_KEY"] = kwargs["upstox_api_key"]
+            os.environ["UPSTOX_API_SECRET"] = kwargs["upstox_api_secret"]
+            os.environ["UPSTOX_REDIRECT_URI"] = kwargs["upstox_redirect_uri"]
 
     def set_fetch_order(self, fetch_order):
         """
@@ -95,7 +104,7 @@ class TickerStore:
                         ticker, start_date, end_date, interval
                     )
                 except SourceError as e:
-                    print(crayons.red("Upstox source error: %s" % e, bold=True))
+                    print(crayons.red("NSE source error: %s" % e, bold=True))
 
     def upstox_historical_data(self, ticker, start_date, end_date, interval):
         """
@@ -235,4 +244,4 @@ class TickerStore:
             )
             return formatted_data
         else:
-            raise SourceError("NSE source: not available for requested interval.")
+            raise SourceError("not available for requested time interval.")
