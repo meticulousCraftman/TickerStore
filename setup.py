229d7d57eccd5372c60c0ffce0c73f5d73f0d03c
Apoorva Singh <apoorvasingh157@gmail.com>
2019-01-07 09:42:32 +0530

    Preparing for packaging

--- setup.py
+++ setup.py
@@ -5,14 +5,23 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="TickerStore",
-    version="0.0.1",
+    version="0.0.2",
     author="Apoorva Singh",
     author_email="apoorva.singh157@gmail.com",
     description="Fetches historical financial data from stock market",
     long_description=long_description,
     url="https://github.com/meticulousCraftman/tickerstore",
     license="MIT",
-    install_requires=["upstox", "python-dotenv", "Flask", "influxdb"],
+    install_requires=[
+        "upstox",
+        "python-dotenv",
+        "Flask",
+        "influxdb",
+        "crayons",
+        "nsepy",
+        "pandas",
+        "requests",
+    ],
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
