be8c9a7c6cf665ff3aefaf0374da18d6efa0d883
Apoorva Singh <apoorvasingh157@gmail.com>
2019-02-22 17:16:26 +0530

    Missed lxml as a dependency for the project.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="TickerStore",
-    version="0.0.4",
+    version="0.0.5",
     author="Apoorva Singh",
     author_email="apoorva.singh157@gmail.com",
     description="Historical data of financial instruments from NSE",
@@ -21,6 +21,7 @@ setuptools.setup(
         "nsepy",
         "pandas",
         "requests",
+        "lxml",
     ],
     packages=setuptools.find_packages(),
     classifiers=[
